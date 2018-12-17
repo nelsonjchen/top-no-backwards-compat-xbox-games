fb433f2b8dd810f54cf28f1888e9a765e48b3bab
Nelson Chen <crazysim@gmail.com>
2018-12-16 23:41:34 -0700

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class MetacriticXbox360GameScoreScraper(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://www.metacritic.com/g00/browse/games/score/metascore/all/xbox360/filtered']
+
+    def parse(self, response):
+        body = response.css('#main > div.module.filter.score_filter > div.module.products_module.list_product_condensed_module > div.body')
+        for product in body.css('li.product'):
+            title_element = product.css('.basic_stat.product_title')
+            title = title_element.css('a ::text').extract_first().strip()
+            user_score = product.css('.product_avguserscore > span.data.textscore ::text').extract_first()
+            # Just multiply it up by 10.
+            user_score = user_score.replace('.', '')
+            critic_score = product.css('.basic_stat.product_score.brief_metascore > div::text').extract_first()
+            yield {
+                'title': title,
+                'user_score': user_score,
+                'critic_score': critic_score,
+            }
+
+        for next_page in response.css('a.action[rel=next]'):
+            yield response.follow(next_page, self.parse)
